e432c76574e12da9749d10ff9a59961befd279f5
Carl Johan Balck <carljohanbalck@carls-macbook.local>
2022-09-18 21:11:58 +0200

    Tutorial template

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("Test")
+func main() {
+	conferenceName := "GO Conference"
+	const confereceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confereceTickets, remainingTickets)
+	fmt.Printf("Get your tickets to attend.\n")
+
+	var firstName string
+	var lastName string
+	var email string
+
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
