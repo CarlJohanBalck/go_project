a5daa9bc2bea906ac8b3a1c0c3b2e954136fa8ab
Carl B <carljohanbalck@gmail.com>
2022-09-23 11:18:01 +0200

    Tutorial template

--- main.go
+++ main.go
@@ -11,10 +11,18 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confereceTickets, remainingTickets)
 	fmt.Printf("Get your tickets to attend.\n")
 
+	var bookings [50]string
+
 	var firstName string
 	var lastName string
 	var email string
 
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v \n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 	var userTickets uint
 
 	fmt.Println("Enter your first name: ")
